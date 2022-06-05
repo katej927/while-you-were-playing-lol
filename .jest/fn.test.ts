055ace5abc85dca30031bff2593ec570ab0e0777
Seonmi Jung <devkatej927@gmail.com>
2022-06-05 15:46:32 +0900

    feat: test utils function's result by jest

---
+++ .jest/fn.test.ts
@@ -0,0 +1,17 @@
+import { convertAllMatch } from '../components/member/utils';
+
+test('convertAllMatch 함수의 결과 값 test', () => {
+  const testArr = [
+    { gameCreation: 1653760118000, gameDuration: 1329, profileIconId: 6 },
+    { gameCreation: 1653672160000, gameDuration: 1734, profileIconId: 6 },
+  ];
+
+  const expectResult = {
+    gameMillisecTime: 3063,
+    playinDate: [
+      { gameCreation: 1653760118000, gameDuration: 1329 },
+      { gameCreation: 1653672160000, gameDuration: 1734 },
+    ],
+  };
+  expect(convertAllMatch(testArr)).toStrictEqual(expectResult);
+});
