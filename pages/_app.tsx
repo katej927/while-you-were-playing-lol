8f5dc6a8a11a02d99c87bce9362594cca65c6eb4
Seonmi Jung <devkatej927@gmail.com>
2022-06-23 15:59:36 +0900

    refactor: revise strategy of GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,10 +12,10 @@ const app = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider>
       <Script
-        strategy='lazyOnload'
+        strategy='afterInteractive'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <Script strategy='lazyOnload'>
+      <Script strategy='afterInteractive'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
