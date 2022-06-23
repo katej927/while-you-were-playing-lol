9a1cc2e468f016e0eafe97a05b3fd11a5d9da506
Seonmi Jung <devkatej927@gmail.com>
2022-06-23 15:56:17 +0900

    remove: useless annotations

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-// import Script from 'next/script'
 
 function Document() {
   return (
     <Html>
       <Head>
         <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests'></meta>
-        <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MY_GOOGLE_MAP_API}`}></script>
-        {/* <Script
-          strategy='lazyOnload'
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script strategy='lazyOnload'>
-          {`
-				      window.dataLayer = window.dataLayer || [];
-      				function gtag(){dataLayer.push(arguments);}
-      				gtag('js', new Date());
-      				gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-      				  page_path: window.location.pathname,
-      				});
-				`}
-        </Script> */}
+        <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MY_GOOGLE_MAP_API}`} />
       </Head>
       <body>
         <Main />
