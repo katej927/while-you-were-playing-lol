21648f53805081293856f07a11e61784002c2a44
Seonmi Jung <devkatej927@gmail.com>
2022-06-26 19:19:49 +0900

    refactor: revise Open Graph

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -18,8 +18,8 @@ const Layout = ({ children }: IProps) => {
     query: { name },
   } = useRouter();
 
-  const titleSet = name ? `${t('common:titleOfApp')} | ${name}` : `${t('common:titleOfApp')}`;
-  const ogTitleSet = name ? `WPL | ${name}` : `WPL | While You Were Playing LoL`;
+  const titleSet = name ? `${t('common:abbreviationTitleOfApp')} | ${name}` : `${t('common:abbreviationTitleOfApp')}`;
+  const ogTitleSet = name ? `${t('common:titleOfApp')} | ${name}` : `${t('common:titleOfApp')}`;
 
   return (
     <>
@@ -28,10 +28,12 @@ const Layout = ({ children }: IProps) => {
         <title>{titleSet}</title>
         <meta property='og:type' content='website' />
         <meta property='og:title' content={ogTitleSet} />
+        <meta property='og:site_name' content={`${t('common:titleOfApp')}`} />
         <meta property='og:description' content={'최근 게임 내역을 검색하여 어떤 기회비용이 있었는지 알려주는 웹앱'} />
-        <meta property='og:image' content='public/static/image' />
+        <meta property='og:image' content='/static/image' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
+        <meta property='og:url' content='https://while-you-were-playing-lol.vercel.app' />
       </Head>
       <div css={S.wrapper}>
         <Header />
