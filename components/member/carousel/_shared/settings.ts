a0f757dba9996c2390189e84b0f1d1c9188f3f13
Seonmi Jung <devkatej927@gmail.com>
2022-07-05 00:33:31 +0900

    refactor: revise responsive(number of slides per screen width) of recent games section

--- components/member/carousel/_shared/settings.ts
+++ components/member/carousel/_shared/settings.ts
@@ -1,31 +1,27 @@
+const breakpoints = [
+  { breakpoint: 820, slidesToShow: 5, slidesToScroll: 5 },
+  { breakpoint: 770, slidesToShow: 4.5, slidesToScroll: 4 },
+  { breakpoint: 690, slidesToShow: 4, slidesToScroll: 4 },
+  { breakpoint: 620, slidesToShow: 3.5, slidesToScroll: 3 },
+  { breakpoint: 540, slidesToShow: 3, slidesToScroll: 3 },
+  { breakpoint: 470, slidesToShow: 2.5, slidesToScroll: 2 },
+  { breakpoint: 400, slidesToShow: 2, slidesToScroll: 2 },
+];
+
 export const settings = {
   arrows: false,
   infinite: false,
   speed: 300,
   slidesToShow: 5.5,
   slidesToScroll: 5,
-  responsive: [
-    {
-      breakpoint: 1024,
+  responsive: breakpoints.map(({ breakpoint, slidesToShow, slidesToScroll }) => {
+    return {
+      breakpoint,
       settings: {
-        slidesToShow: 4.5,
-        slidesToScroll: 4,
+        slidesToShow,
+        slidesToScroll,
         infinite: false,
       },
-    },
-    {
-      breakpoint: 690,
-      settings: {
-        slidesToShow: 2.5,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 460,
-      settings: {
-        slidesToShow: 1.5,
-        slidesToScroll: 1,
-      },
-    },
-  ],
+    };
+  }),
 };
